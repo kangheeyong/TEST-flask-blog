c28c45103596b3f7ca2af91dc084fbf7e1fb2eea
강희용 <hykang@allganize.io>
2020-07-17 23:05:43 +0900

    add docker

---
+++ web/dockerfile_generator.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+    dockerfile = '''FROM python:3.8
+
+RUN pip install --upgrade pip
+'''
+    for l in open('requirements.txt'):
+        l = l.strip()
+        if not l or l.startswith('#'):
+            continue
+        dockerfile += "RUN pip install {}\n".format(l)
+
+    dockerfile += '''
+RUN mkdir -p /app
+
+WORKDIR /app
+'''
+    with open('Dockerfile', 'w') as file :
+        file.write(dockerfile)
