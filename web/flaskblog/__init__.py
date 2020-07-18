4457b6c0391206f28d8a97e8b1964ee2e5ec0c0b
강희용 <hykang@allganize.io>
2020-07-18 15:33:46 +0900

    add image and save

--- web/flaskblog/__init__.py
+++ web/flaskblog/__init__.py
@@ -4,11 +4,15 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'dsflkj32jd9ejlkj3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
